e31e98386f7b87ac2a781b2647a26e072137d978
Dario Gutierrez Lasso <dariojgl0211@gmail.com>
2023-05-16 12:25:32 -0500

    cambio 7

--- js/2ley.js
+++ js/2ley.js
@@ -10,7 +10,7 @@ function calcular() {
   } else if (masa === "" && fuerza !== "" && aceleracion !== "") {
     resultado = `Masa = ${fuerza / aceleracion} kg`;
   } else if (aceleracion === "" && fuerza !== "" && masa !== "") {
-    resultado = `Aceleración = ${fuerza / masa} m/s^2`;
+    resultado = `Aceleración = ${fuerza / masa} m/s²`;
   } else {
     resultado = "Introduce dos valores para calcular el tercero";
   }
